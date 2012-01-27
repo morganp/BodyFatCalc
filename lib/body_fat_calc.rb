fb95be0cb7a01ac837c7559b94181c3587a3f5f8
Morgan Prior <morgan_prior@yahoo.co.uk>
2012-01-27 08:42:57 +0000

    required the bmi class

--- lib/body_fat_calc.rb
+++ lib/body_fat_calc.rb
@@ -1,15 +1,16 @@
-
 module BodyFatCalc
   VERSION = '0.0.1'
 end
 
 begin
   require_relative 'body_fat_calc/measurments'
+  require_relative 'body_fat_calc/bmi'
   require_relative 'body_fat_calc/jackson_pollock_3'
   require_relative 'body_fat_calc/jackson_pollock_7'
   require_relative 'body_fat_calc/parillio_9'
 rescue
   require 'body_fat_calc/measurments'
+  require 'body_fat_calc/bmi'
   require 'body_fat_calc/jackson_pollock_3'
   require 'body_fat_calc/jackson_pollock_7'
   require 'body_fat_calc/parillio_9'
