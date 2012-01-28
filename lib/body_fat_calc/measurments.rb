2ecb83612b402ac3f375c220a1844d88381c4fd1
Morgan Prior <morgan@amaras-tech.co.uk>
2012-01-28 16:04:29 +0000

    Adding Methods to the body fat calculations

--- lib/body_fat_calc/measurments.rb
+++ lib/body_fat_calc/measurments.rb
@@ -14,5 +14,25 @@ require 'ostruct'
 module BodyFatCalc
   class Measurments < OpenStruct
 
+
+      def bmi
+        BMI.new( self ).body_fat
+      end
+
+      def jackson_pollock_3
+        JacksonPollock3.new( self ).body_fat
+      end
+
+      def jackson_pollock_3_brozek
+        JacksonPollock3.new( self, :brozek ).body_fat
+      end
+
+      def jackson_pollock_3_siri
+        JacksonPollock3.new( self, :siri ).body_fat
+      end
+      def parillio_9
+        Parillio9.new( self ).body_fat
+      end
+
   end
 end
