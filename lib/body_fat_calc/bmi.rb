bc01ce91185b074521d9d9917181d94340424819
Morgan Prior <morgan@amaras-tech.co.uk>
2012-01-28 16:03:39 +0000

    Added Validate method to BMI

--- lib/body_fat_calc/bmi.rb
+++ lib/body_fat_calc/bmi.rb
@@ -4,9 +4,32 @@ module BodyFatCalc
     def initialize( measurments )
       @measurments = measurments
     end
-    
+
+    def validate
+      errors = []
+
+      if @measurments.weight.nil?
+        errors << "weight"
+      end
+
+      if @measurments.height.nil?
+        errors << "height"
+      end
+
+      if errors.any?
+        $stderr.puts "BMI requires " + errors.join(', ')
+      end
+
+      return errors.empty?
+    end
+
+
     def body_fat
-      ( @measurments.weight.to_f / ( @measurments.height**2 ) )
+      if validate
+        ( @measurments.weight.to_f / ( @measurments.height**2 ) )
+      else
+        -1
+      end
     end
 
   end
