9586dd00539b2ad75e2742c3f9f935a0fbcf2591
Morgan Prior <morgan@amaras-tech.co.uk>
2012-01-28 16:05:37 +0000

    Adding specs for new methods, body_fat calcs built in to measurments

--- spec/body_fat_calc_spec.rb
+++ spec/body_fat_calc_spec.rb
@@ -2,23 +2,43 @@
 require 'spec_helper'
 
 describe BodyFatCalc do
-  it "should do something" do
+  it " Fail nicely when no data supplied " do
+    measurments = BodyFatCalc::Measurments.new({
+    })
+    measurments.bmi.should                      < 0
+    measurments.jackson_pollock_3.should        < 0
+    measurments.parillio_9.should               < 0
+  end
+  
+  it " Fail nicely when only weight supplied " do
+    measurments = BodyFatCalc::Measurments.new({
+      :weight    => 111.5,
+    })
+    measurments.bmi.should                      < 0
+    measurments.jackson_pollock_3.should        < 0
+    measurments.parillio_9.should               < 0
+  end
+  
+    
+  it " Calculate BMI and Jackson Pollock 3 " do
     measurments = BodyFatCalc::Measurments.new({
       :sex       => 'male',
+      :weight    => 111.5,
+      :height    => 1.80,
       :age       => 28,
       :chest     => 21,
       :abdominal => 47,
       :thigh     => 43
     })
 
-    pollock3 = BodyFatCalc::JacksonPollock3.new( measurments )
-    pollock3.body_fat_siri.should == 30.52528111457019
-    pollock3.body_fat_brozek.should == 29.43647165526979
+    measurments.bmi.should                      == 34.413580246913575
+    measurments.jackson_pollock_3.should        == 29.43647165526979
+    measurments.jackson_pollock_3_brozek.should == 29.43647165526979
+    measurments.jackson_pollock_3_siri.should   == 30.52528111457019
+    #measurments.parillio_9.should               < 0
   end
-end
 
-describe BodyFatCalc do
-  it "should do something" do
+  it " Jackson Pollock 3 Male Only " do
     measurments = BodyFatCalc::Measurments.new({
       :sex       => 'male',
       :age       => 28,
@@ -27,9 +47,10 @@ describe BodyFatCalc do
       :thigh     => 33.5
     })
 
-    pollock3 = BodyFatCalc::JacksonPollock3.new( measurments )
-    pollock3.body_fat_siri.should == 21.536980025693595
-    pollock3.body_fat_brozek.should == 21.13818155907463
+    measurments.bmi.should                      == -1
+    measurments.jackson_pollock_3.should        == 21.13818155907463
+    measurments.jackson_pollock_3_brozek.should == 21.13818155907463
+    measurments.jackson_pollock_3_siri.should   == 21.536980025693595 
   end
 end
 
